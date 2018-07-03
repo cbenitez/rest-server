f96ae87df761ee332b7aa16265d4293f17754439
Christian Benitez <cbenitez@sd.com.py>
2018-07-03 18:26:48 -0400

    commit 2

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
-app.get('/usuario', ( req,res ) => {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/usuario', ( req, res ) => {
 	res.json('get Usuario');
 });
 
-app.post('/usuario', (req, res) => {
-	res.json('post Usuario');
+app.post('/usuario', ( req, res ) => {
+	
+	if(!req.body) return res.sendStatus(400);
+	let nombre = req.body.nombre;
+	let edad = req.body.edad;
+	res.json({nombre,edad});
 });
 
 app.put('/usuario/:id', (req, res) => {
