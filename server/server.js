0c8701423f5482441532cb062bc72774af7e6c3d
cbenitez10 <cbenitezdiaz@gmail.com>
2018-07-08 22:35:08 -0400

    usuario

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 require('./config/config');
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const port = process.env.PORT;
+const express 		= require('express');
+const mongoose 		= require('mongoose');
+const bodyParser 	= require('body-parser');
+const app 			= express();
+const port 			= process.env.PORT;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,40 +12,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', ( req, res ) => {
-	res.json('get Usuario');
+app.use( require( './routes/usuario') );
+
+mongoose.connect('mongodb://localhost:27017/super_cafe', ( err, res ) => {
+	if( err ) throw err;
+	console.log( 'Base de datos Online!' );
 });
 
-app.post('/usuario', ( req, res ) => {
-	
-	let body = req.body;
-
-	if( body.nombre === undefined ){
-		res.status(400).json({
-			ok: false,
-			mensaje: 'El nombre es necesario'
-		});
-	}else{
-		res.json({
-			persona:body
-		})
-	}
-});
-
-app.put('/usuario/:id', (req, res) => {
-
-	let id = req.params.id;
-
-	res.json({id});
-});
-
-app.delete('/usuario/:id', (req, res) => {
-	
-	let id = req.params.id;
-	
-	res.json({id});
-});
-
-app.listen( port, ()=>{
-	console.log(`Puerto ${ port }`);
+app.listen( port, () => {
+	console.log( `Puerto ${ port }` );
 });
